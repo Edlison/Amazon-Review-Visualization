2509db894762b38fde51a62b59b3f88c5fdae2b4
Edlison <bolin.edlison.shen@gmail.com>
2023-04-10 02:24:53 -0400

    init repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# Amazon Review Dataset Visualization
+
+Dataset: [link](https://cseweb.ucsd.edu/~jmcauley/datasets/amazon_v2/)
+
+## Data Sources
+Number of Users: 5523
+
+Number of Items: 17389
+
+Number of Interactions: 496315
+
+Number of Brands: 330
+
+Number of Categories: 149
+
---
+++ main.py
@@ -0,0 +1,18 @@
+# @Author  : Edlison
+# @Date    : 3/13/23 19:18
+import dgl
+from pyvis.network import Network
+import networkx as nx
+
+dataset = dgl.data.CoraGraphDataset()
+
+g = Network(height=800, width=800, notebook=True)
+
+netxG = nx.Graph(dataset[0].to_networkx())
+
+mapping = {i:i for i in range(netxG.size())} #Setting mapping for the relabeling
+netxH = nx.relabel_nodes(netxG,mapping) #relabeling nodes
+
+g.from_nx(netxH)
+g.show('ex.html')
+
